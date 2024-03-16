f0afcee3770ac247b44057cd70c7f475685806a2
ItzTas <talessabini2@gmail.com>
2024-03-16 14:20:32 -0300

    First files of static site generator

---
+++ public/main.sh
@@ -0,0 +1,1 @@
+python /home/talinux/workspace/github.com/itzTas/public/src/main.py
---
+++ public/src/main.py
@@ -0,0 +1,5 @@
+from textnode import TextNode
+
+Text = TextNode("This is a text node", "bold", "https://www.boot.dev")
+
+print(Text)
