3e857b02f936d5340366681c63e9d28aafe8214d
ItzTas <talessabini2@gmail.com>
2024-03-17 22:16:54 -0300

    I added a LeafNode class that inherits from the HTMLNode class and I created tests with the to_html function withing this class

--- public/src/htmlnode.py
+++ public/src/htmlnode.py
@@ -9,6 +9,8 @@ class HTMLNode():
         raise NotImplementedError
 
     def props_to_html(self):
+        if not self.props:
+            return ""
         html_atributes_str = ""
         for key, value in self.props.items():
             html_atributes_str += f" {key}=\"{value}\""
@@ -21,4 +23,18 @@ class HTMLNode():
               Children: {self.children} \n \
               Props: {self.props} \
               "
-              
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag=tag, value=value, children=None,props=props)
+        
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leafs nodes require a value")
+        elif self.tag == None:
+            return self.value
+        elif self.tag != None:
+            return f"<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>"
+        else:
+            raise Exception("Something went wrong")
+        
--- public/src/test_htmlnode.py
+++ public/src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode (unittest.TestCase):
     def test_repr(self):
@@ -17,3 +17,29 @@ class TestHTMLNode (unittest.TestCase):
         node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank", "width": "150px"})
         expected_result = ' href="https://www.google.com" target="_blank" width="150px"'
         self.assertEqual(node.props_to_html(), expected_result)
+        
+    def test_leaf_node_value_none(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode(value=None)
+            node.to_html()
+            
+    def test_leaf_node_tag_none(self):
+        node = LeafNode(value="test",tag=None)
+        expected = "test"
+        result = node.to_html()
+        self.assertEqual(result, expected)
+        
+    def test_leaf_node_tag_not_none_no_props(self):
+        node = LeafNode(value="test",tag="p")
+        result = node.to_html()
+        expected = "<p>test</p>"
+        self.assertEqual(result, expected)
+    
+    def test_leaf_node_tag_not_none_with_props(self):
+        node = LeafNode(value="test", tag="a", props={"href": "test"})
+        result = node.to_html()
+        expected = "<a href=\"test\">test</a>"
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
