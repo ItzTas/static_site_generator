46e5150ec1aff960ab749d2d0fc4d1593a529847
ItzTas <talessabini2@gmail.com>
2024-03-17 21:09:08 -0300

    test_props_to_html

--- public/src/test_htmlnode.py
+++ public/src/test_htmlnode.py
@@ -4,8 +4,8 @@ from htmlnode import HTMLNode
 
 class TestHTMLNode (unittest.TestCase):
     def test_repr(self):
-        node1 = HTMLNode("<h1>", "this is a test")
-        self.assertEqual(repr(node1),
+        node = HTMLNode("<h1>", "this is a test")
+        self.assertEqual(repr(node),
               "Class name: HTMLNode\n \
               Tag: <h1> \n \
               Value: this is a test \n \
@@ -13,3 +13,7 @@ class TestHTMLNode (unittest.TestCase):
               Props: None \
               ")
     
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank", "width": "150px"})
+        expected_result = ' href="https://www.google.com" target="_blank" width="150px"'
+        self.assertEqual(node.props_to_html(), expected_result)
